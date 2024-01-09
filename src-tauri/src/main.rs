aeead803c7b7cf4945255a866231a68f1f925089
porum <sunguobao12@gmail.com>
2024-01-09 18:14:43 +0800

    support ubuntu amd64 arch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -48,30 +48,35 @@ fn show_in_folder(path: String, opening: bool) {
 
   #[cfg(target_os = "linux")]
   {
-    if path.contains(",") {
-      // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
-      let new_path = match metadata(&path).unwrap().is_dir() {
-        true => path,
-        false => {
-          let mut path2 = PathBuf::from(path);
-          path2.pop();
-          path2.into_os_string().into_string().unwrap()
-        }
-      };
-      Command::new("xdg-open")
-        .arg(&new_path)
+    Command::new("nautilus")
+        .arg(&path)
         .spawn()
         .unwrap();
-    } else {
-      if let Ok(Fork::Child) = daemon(false, false) {
-        Command::new("dbus-send")
-          .args(["--session", "--dest=org.freedesktop.FileManager1", "--type=method_call",
-            "/org/freedesktop/FileManager1", "org.freedesktop.FileManager1.ShowItems",
-            format!("array:string:\"file://{path}\"").as_str(), "string:\"\""])
-          .spawn()
-          .unwrap();
-      }
-    }
+
+    // if path.contains(",") {
+    //   // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
+    //   let new_path = match metadata(&path).unwrap().is_dir() {
+    //     true => path,
+    //     false => {
+    //       let mut path2 = PathBuf::from(path);
+    //       path2.pop();
+    //       path2.into_os_string().into_string().unwrap()
+    //     }
+    //   };
+    //   Command::new("xdg-open")
+    //     .arg(&new_path)
+    //     .spawn()
+    //     .unwrap();
+    // } else {
+    //   if let Ok(Fork::Child) = daemon(false, false) {
+    //     Command::new("dbus-send")
+    //       .args(["--session", "--dest=org.freedesktop.FileManager1", "--type=method_call",
+    //         "/org/freedesktop/FileManager1", "org.freedesktop.FileManager1.ShowItems",
+    //         format!("array:string:\"file://{path}\"").as_str(), "string:\"\""])
+    //       .spawn()
+    //       .unwrap();
+    //   }
+    // }
   }
 
   #[cfg(target_os = "macos")]
